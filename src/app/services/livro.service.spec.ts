baa2f8d0475b700ab34597ce80912ff821b0129d
Andressa Turchetto <and.turchetto@gmail.com>
2024-08-31 23:01:42 -0300

    Aprendendo e desenvolvendo os primeiros testes com Jest

---
+++ src/app/services/livro.service.spec.ts
@@ -0,0 +1,38 @@
+import { Livro } from "../componentes/livro/livro";
+import { livros } from "../mock-livros";
+import { LivroService } from "./livro.service"
+
+describe ('LivroService', () => {
+  let service: LivroService;
+
+  it('deveria ser criado', () => {
+    service = new LivroService();
+    expect(service).toBeTruthy();
+  })
+
+  //AAA (ARRANGE ACT ASSERT)
+
+  it('deveria adicionar um novo livro', () => {
+    service = new LivroService();
+    const novoLivro: Livro = {
+        titulo: 'Novo Livro',
+        autoria: 'Autor Desconhecido',
+        imagem: 'http://example.com/cover.jpg',
+        genero: {id: 'romance', value: 'Romance'},
+        dataLeitura: '2024-04-19',
+        classificacao: 5
+    };
+
+    service.adicionarLivro(novoLivro);
+    const livrosPorGenero = service.obterLivrosPorGenero('romance');
+    expect(livrosPorGenero).toContain(novoLivro);
+  });
+
+  it('deveria recuperar corretamento os livros por gênero', () => {
+    service = new LivroService();
+
+    const livrosPorGenero = service.obterLivrosPorGenero('romance');
+    const livrosEsperados = livros.filter(livro => livro.genero.id === 'romance');
+    expect(livrosPorGenero).toEqual(livrosEsperados);
+  })
+})
