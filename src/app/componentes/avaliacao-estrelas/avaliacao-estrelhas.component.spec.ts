4a7a91f10d6cbeb38d30f1083ba5910936484574
Consórcio ASSES - COMPUTADORES <computadores@consorcioasses.onmicrosoft.com>
2024-09-13 15:17:19 -0300

    Trabalhando com testes

---
+++ src/app/componentes/avaliacao-estrelas/avaliacao-estrelhas.component.spec.ts
@@ -0,0 +1,65 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AvaliacaoEstrelasComponent } from './avaliacao-estrelas.component';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { forwardRef } from '@angular/core';
+
+describe('AvaliacaoEstrelasComponent', () => {
+  let component: AvaliacaoEstrelasComponent;
+  let fixture: ComponentFixture<AvaliacaoEstrelasComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [AvaliacaoEstrelasComponent],
+        providers: [
+            {
+              provide: NG_VALUE_ACCESSOR,
+              useExisting: forwardRef(() => AvaliacaoEstrelasComponent),
+              multi: true
+            }
+          ]
+    });
+
+    fixture = TestBed.createComponent(AvaliacaoEstrelasComponent);
+    component = fixture.componentInstance;
+    component.readOnly = false;
+  });
+
+  it('deveria ser criado', () => {
+    expect(component).toBeTruthy();
+  })
+
+  it('deveria atribuir um valor para a classificação quando o método writeValue for chamado', () => {
+    const classificacao = 3;
+    component.writeValue(classificacao);
+    
+    expect(component.classificacao).toBe(classificacao);
+  });
+
+  it('deveria chamar o onChange quando o método classificar for chamado', () => {
+    const onChangeSpy = jest.spyOn(component, 'onChange');
+    const classificacao = 4;
+    component.classificar(classificacao);
+    
+    expect(onChangeSpy).toHaveBeenCalled();
+  });
+
+  it('deveria chamar o onTouched quando o método classificar for chamado', () => {
+    const onTouched = jest.spyOn(component, 'onTouched');
+    const classificacao = 4;
+    component.classificar(classificacao);
+    
+    expect(onTouched).toHaveBeenCalled();
+  });
+
+  it('não deveria atualizar a classificação quando a propriedade readonly for true', () => {
+    const onChangeSpy = jest.spyOn(component, 'onChange');
+    component.readOnly = true;
+    const classificacao = 5;
+    component.classificar(classificacao);
+    
+    expect(onChangeSpy).not.toHaveBeenCalled();
+    expect(component.classificacao).not.toBe(classificacao);
+  });
+
+})
+
--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -1,13 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CabecalhoComponent } from "./cabecalho.component"
 
 describe('CabecalhoComponent', () => {
-    let component: CabecalhoComponent
+    let component: CabecalhoComponent;
+    let fixture: ComponentFixture<CabecalhoComponent>;
 
-    beforeEach(() =>{
-        component = new CabecalhoComponent
-    });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+          imports: [CabecalhoComponent],
+        })
+        fixture = TestBed.createComponent(CabecalhoComponent);
+        component = fixture.componentInstance;
+      });
 
     it('deveria ser criado', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('deveria definir as propriedades alt e src', () => {
+        expect(component.alt).toBeDefined();
+        expect(component.src).toBeDefined();
+
+    })
+
+    it('deveria renderizar o conteudo baseado nas propriedades src e alt', () => {
+        component.src = 'https://example.com/test-image.jpg';
+        component.alt = 'Imagem teste';
+        expect(component).toMatchSnapshot();
     })
 })
