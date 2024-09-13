338fb5b8841eace872443e5c1e3adb5967dde4f6
Consórcio ASSES - COMPUTADORES <computadores@consorcioasses.onmicrosoft.com>
2024-09-13 11:20:07 -0300

    Adicionando testes

--- src/app/services/livro.service.spec.ts
+++ src/app/services/livro.service.spec.ts
@@ -1,12 +1,15 @@
-import { Livro } from "../componentes/livro/livro";
+import { GeneroLiterario, Livro } from "../componentes/livro/livro";
 import { livros } from "../mock-livros";
-import { LivroService } from "./livro.service"
+import { ErroGeneroLiterario, LivroService } from "./livro.service"
 
 describe ('LivroService', () => {
   let service: LivroService;
 
-  it('deveria ser criado', () => {
+  beforeEach(() => {
     service = new LivroService();
+  })
+
+  it('deveria ser criado', () => {
     expect(service).toBeTruthy();
   })
 
@@ -34,5 +37,45 @@ describe ('LivroService', () => {
     const livrosPorGenero = service.obterLivrosPorGenero('romance');
     const livrosEsperados = livros.filter(livro => livro.genero.id === 'romance');
     expect(livrosPorGenero).toEqual(livrosEsperados);
+  });
+
+  it('deveria inicializar os generos corretamente', () => {
+    const generosEsperados: GeneroLiterario[] = [
+      {
+        id: 'romance',
+        value: 'Romance'
+      },
+      {
+        id: 'misterio',
+        value: 'Mistério'
+      },
+      {
+        id: 'fantasia',
+        value: 'Fantasia'
+      },
+      {
+        id: 'ficcao-cientifica',
+        value: 'Ficção Científica'
+      },
+      {
+        id: 'tecnicos',
+        value: 'Técnicos'
+      },
+    ];
+
+    expect(service.generos).toEqual(generosEsperados);
+  });
+
+  it('deveria lançar um erro ao tentar cadastrar um livro com gênero desconhecido', () => {
+    const novoLivro: Livro = {
+        titulo: 'Novo Livro',
+        autoria: 'Autor Desconhecido',
+        imagem: 'http://example.com/cover.jpg',
+        genero: {id: 'nao-existe', value: 'Não Existe'},
+        dataLeitura: '2024-04-19',
+        classificacao: 5
+    };
+
+    expect(() => service.adicionarLivro(novoLivro)).toThrow(ErroGeneroLiterario);
   })
 })
